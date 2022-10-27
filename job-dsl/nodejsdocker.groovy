1ace830ad0bd5c91c275288aa9eab24d4413d4f2
Nitin Shuka <nshukla6@gmail.com>
2022-10-27 23:31:30 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,7 +1,7 @@
 job('NodeJS Docker example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
+            node / gitConfigName('Basu')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('nshukla6/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
